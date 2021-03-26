09abde2ac5f34427d209058b35e809b91ff0b515
Elbert Bae <elbert.bae@gmail.com>
2021-03-25 22:00:46 -0700

    Remove unused imports

--- src/features/article-container/article-container.tsx
+++ src/features/article-container/article-container.tsx
@@ -4,7 +4,6 @@ import Box from "@material-ui/core/Box";
 import { makeStyles } from "@material-ui/core/styles";
 import Content from "components/content";
 import CodeBlock from "components/code-block";
-import { drawerWidth } from "components/menu-drawer";
 
 const useStyles = makeStyles((theme) => ({
   articleContainer: {
